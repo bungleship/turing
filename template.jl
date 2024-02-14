e652984a6ec499fe4d13a0ded334c34b75da4903
bungleship <131033590+bungleship@users.noreply.github.com>
2024-02-14 10:07:02 -0800

    Add files via upload

---
+++ template.jl
@@ -0,0 +1,39 @@
+# Template
+# Author: bungleship
+# Info: stuff
+
+using Turing, CairoMakie
+
+mutable struct Posterior
+    model
+    chains
+    plot
+    @model function observe(data, priors)
+
+	# Define priors
+	p ~ priors
+
+	# Condition on data
+	data ~ Binomial(100, p)
+    end
+    function Posterior(data, priors=Beta(1,1))
+	# fitting the model
+	model = observe(data, priors)
+	
+	# sampling
+	num_chains = 1
+	chains = sample(model, MH(), MCMCThreads(), 40000, num_chains)
+
+	# graphing
+	function plot()
+	    # all values, parameter p, first chain
+	    density(post.chains[:, :p, 1])
+	end
+	new(model, chains, plot)
+    end
+end
+
+# Usage
+# observing 61 heads out of 100 with a uniform continuous prior for p
+# post = Posterior(61, Beta(1,1))
+
